7953707d8e09e478d8a8bbd790548d1c4ee5e5dc
AmrA-Ali <amrabdelghaniali@aucegypt.edu>
2017-05-28 21:46:06 +0200

    Sheyaka Countdown321

---
+++ Assets/DoneSave.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class DoneSave : MonoBehaviour {
+    [SerializeField]
+    private Text mapName;
+	void Start () {
+        GetComponent<Button>().onClick.AddListener(delegate { Confirm(); });
+	}
+    void Confirm()  {
+        gameObject.map().info.name = mapName.text;
+        gameObject.map().Save();
+    }
+}
